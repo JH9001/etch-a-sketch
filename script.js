4d98fbd430729af4e9edd05df08eafa8d6d0749e
xxfsd <josh.fsd@protonmail.com>
2022-05-12 16:23:10 +1000

    Create dynamic grid, create buttons, position items with flexbox

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const grid = document.querySelector('.grid');
+let gridContainer = document.querySelector('.gridContainer');
+
+//could do input
+let userInput = 16;
+
+const createGrid = function () {
+  for (let i = 0; i < userInput * userInput; i++) {
+    let gridDivs = document.createElement('div');
+    gridDivs.className = 'blocks';
+    gridDivs.textContent = '';
+    gridContainer.appendChild(gridDivs);
+    gridContainer.style = `grid-template-rows: repeat(${userInput}, 1fr)`;
+    gridContainer.style = `grid-template-columns: repeat(${userInput}, 1fr)`;
+  }
+};
+
+createGrid();
