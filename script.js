39206c1a8ddf7bb33baf6762f2325aec632fdee9
xxfsd <josh.fsd@protonmail.com>
2022-05-17 16:57:38 +1000

    Create slider for user input that dynamically resizes grid

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-let gridContainer = document.querySelector('.gridContainer');
+//grid container
+const gridContainer = document.querySelector('.gridContainer');
 
-//could do input
-let userInput = 16;
+//buttons
+const colorSelect = document.querySelector('.colorSelect')
+const rainbowButton = document.querySelector('.rainbowButton');
+const eraserButton = document.querySelector('.eraserButton');
+const slider = document.querySelector('.slider');
+const clearButton = document.querySelector('.clearButton');
 
-const createGrid = function () {
-  for (let i = 0; i < userInput * userInput; i++) {
-    let gridDivs = document.createElement('div');
+//user select slider value which creates grid size
+slider.addEventListener('change', function() {
+  gridContainer.innerHTML = '';
+  createGrid();
+});
+
+//create grid from slider value
+const createGrid = () => {
+  for (let i = 0; i < slider.value * slider.value; i++) {
+    const gridDivs = document.createElement('div');
     gridDivs.className = 'blocks';
     gridDivs.textContent = '';
     gridContainer.appendChild(gridDivs);
-    gridContainer.style = `grid-template-rows: repeat(${userInput}, 1fr)`;
-    gridContainer.style = `grid-template-columns: repeat(${userInput}, 1fr)`;
+    gridContainer.style = `grid-template-rows: repeat(${slider.value}, 1fr)`;
+    gridContainer.style = `grid-template-columns: repeat(${slider.value}, 1fr)`;
   }
 };
 
 createGrid();
+
+//change block color on hover
+gridContainer.addEventListener('mouseover', function (e) {
+  if (e.target.className == 'blocks') {
+    e.target.style.backgroundColor = colorSelect.value;
+  }
+});
